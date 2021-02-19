25f407f1c3b8f694cb151bd44aa844f2eebe58d7
rafdude <79229372+rafdude@users.noreply.github.com>
2021-02-19 15:34:32 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-
-echo
-
 echo ''...................................######..########....###....########..########.................................''
 echo ''..##...##...##...##...##...##.....##....##....##......##.##...##.....##....##........##...##...##...##...##...##.''
 echo ''...##.##.....##.##.....##.##......##..........##.....##...##..##.....##....##.........##.##.....##.##.....##.##..''
@@ -21,41 +17,41 @@ echo "Current time: $now"
 echo 
 echo 
 
-
-echo "Step 1: pre-configuring packages"
+echo "Step 1: Pre-configuring packages"
 sudo dpkg --configure -a
 
 echo
 
-echo "Step 2: fix and attempt to correct a system with broken dependencies"
+echo "Step 2: Fix and attempt to correct a system with broken dependencies"
 sudo apt-get install -f
 
 echo
 
-echo "Step 3: update apt cache"
+echo "Step 3: Update apt cache"
 sudo apt-get update
 
 echo
 
-echo "Step 4: upgrade packages"
+echo "Step 4: Upgrade packages"
 sudo apt-get -y upgrade
 
 echo
 
-echo "Step 5: distribution upgrade"
+echo "Step 5: Distribution upgrade"
 sudo apt-get -y dist-upgrade
 
 echo
 
-echo "Step 6: remove unused packages"
+echo "Step 6: Remove unused packages"
 sudo apt-get -y --purge autoremove
 
 echo
 
-echo "Step 7: clean up"
+echo "Step 7: Clean up"
 sudo apt-get -y autoclean
-
 
 now=$(date)
 echo "Current time: $now"                                                       
-                                                                    
+   
+echo "Step 8: Rebooting if needed..."                                                                 
+needrestart -r i
