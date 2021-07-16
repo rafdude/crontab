c3b47154ba623579b9a2c4e55267b43d119898dd
rafdude <79229372+rafdude@users.noreply.github.com>
2021-07-15 22:27:04 -0400

    Update history.sh

--- history.sh
+++ history.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-history -c && history -w
+cat /dev/null > ~/.bash_history && history -c
